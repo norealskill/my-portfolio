8f82d0c65206dc6365c800eb5f3cdf2e0bbddcec
Clinton Cauley <clint.cauley@gmail.com>
2025-02-17 22:06:20 -0500

    neondb deployed, connection and data feed working. Beginning to work with UI elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <div></div>;
+// Next.js example
+import { neon } from '@neondatabase/serverless';
+
+async function getData() {
+  const sql = neon(process.env.DATABASE_URL!);
+  const response = await sql`select * from users`;
+  return response;
+}
+
+export default async function Home() {
+  const data = await getData();
+
+  return (
+    <ul role="list" className="divide-y divide-gray-100">
+      {data.map((row) => {
+        return (
+          <li key={row.user_id} className="flex justify-between gap-x-6 py-5">
+            {row.first_name}
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
